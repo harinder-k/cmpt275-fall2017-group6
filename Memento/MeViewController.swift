1d02d4a5e966a762d2f3926fafc309969026afa2
hkhakh <hkhakh@lib3000-icmac22.ucslabs.sfu.ca>
2017-10-27 12:21:39 -0700

    Added navigation controller

---
+++ Memento/MeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MeViewController.swift
+//  Memento
+//
+//  Created by hkhakh on 10/27/17.
+//  Copyright © 2017 memTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MeViewController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Memento/StoriesViewControler.swift
@@ -0,0 +1,16 @@
+//
+//  StoriesViewControler.swift
+//  Memento
+//
+//  Created by hkhakh on 10/27/17.
+//  Copyright © 2017 memTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StoriesViewController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
