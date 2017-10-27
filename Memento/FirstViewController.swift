24cf105db4a44f7f1f5fd41e88d07ed5af32f237
ama121 <abhishekmahajan19@ymail.com>
2017-10-26 17:32:48 -0700

    Replace

---
+++ Memento/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Memento
+//
+//  Created by Abhishek Mahajan on 2017-10-26.
+//  Copyright © 2017 memTech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
