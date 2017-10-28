565a8dd6b0ead23469c7bb4dd1c622256092a22a
hkhakh <hkhakh@lib3000-icmac10.ucslabs.sfu.ca>
2017-10-27 18:56:45 -0700

    Added login pages and navigation functionality

---
+++ Memento/StoriesNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  StoriesNavigationController.swift
+//  Memento
+//
+//  Created by hkhakh on 10/27/17.
+//  Copyright © 2017 memTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StoriesNavigationController:UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
