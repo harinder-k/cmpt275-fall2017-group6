38246d040f9bad85eb5eb195f00cd45823c383fe
hkhakh <hkhakh@asb9700-f02.csil.sfu.ca>
2017-10-31 11:49:14 -0700

    creating branch

--- Memento/QuizzesViewController.swift
+++ Memento/QuizzesViewController.swift
@@ -14,6 +14,7 @@ class QuizzesViewController:UIViewController {
         super.viewDidLoad()
     }
     
+    
     @IBAction func animalsButtonTapped(_ sender: Any) {
         let takeQuizVC = storyboard?.instantiateViewController(withIdentifier: "TakeQuizViewController") as! TakeQuizViewController
         takeQuizVC.stringPassed = "Animals Quiz"
