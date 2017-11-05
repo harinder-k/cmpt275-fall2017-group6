508366c817f49627facf08bce1b717f57cf341d0
twoinosk <twoinosk@asb9700-e06.csil.sfu.ca>
2017-11-04 21:59:11 -0700

    Added notification for creating story

--- Memento/StoriesViewControler.swift
+++ Memento/StoriesViewControler.swift
@@ -13,17 +13,20 @@ class StoriesViewController:UIViewController {
     
     
     @IBAction func NewStoryButton(_ sender: Any) {
-        //Notification:
         let storyNameAlert = UIAlertController(title: "Create A New Story", message: "What would like like to call your story?", preferredStyle: UIAlertControllerStyle.alert)
         
         storyNameAlert.addTextField { (textField) in textField.text = "Name"
         }
+        let moveToCreateStory = UIAlertAction(title: "Okay", style: .default){
+            [weak storyNameAlert] (_) in let textField = storyNameAlert?.textFields![0]
+        }
         
-        storyNameAlert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { [weak storyNameAlert] (_) in
-            let textField = storyNameAlert?.textFields![0]
-        }))
+        storyNameAlert.addAction(moveToCreateStory)
         
-        self.present(storyNameAlert, animated: true, completion: nil)
+        self.present(storyNameAlert, animated: true){
+            () -> Void in print("alert presented")
+        }
+        self.view
     }
     
     override func viewDidLoad() {
