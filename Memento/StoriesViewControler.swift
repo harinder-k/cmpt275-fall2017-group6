f7c8af2957ef1c73e46e4978371eb252cc4efbf3
twoinosk <twoinosk@asb9700-e06.csil.sfu.ca>
2017-11-04 21:25:00 -0700

    started my own branch

--- Memento/StoriesViewControler.swift
+++ Memento/StoriesViewControler.swift
@@ -10,6 +10,22 @@ import Foundation
 import UIKit
 
 class StoriesViewController:UIViewController {
+    
+    
+    @IBAction func NewStoryButton(_ sender: Any) {
+        //Notification:
+        let storyNameAlert = UIAlertController(title: "Create A New Story", message: "What would like like to call your story?", preferredStyle: UIAlertControllerStyle.alert)
+        
+        storyNameAlert.addTextField { (textField) in textField.text = "Name"
+        }
+        
+        storyNameAlert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { [weak storyNameAlert] (_) in
+            let textField = storyNameAlert?.textFields![0]
+        }))
+        
+        self.present(storyNameAlert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
