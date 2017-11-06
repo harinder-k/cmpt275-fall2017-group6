c7daf934bb7b6daaa43dead8f7cf9f783e33b75c
twoinosk <twoinosk@asb9700-e01.csil.sfu.ca>
2017-11-06 12:28:06 -0800

    Added Matts part to the app

---
+++ Memento/MemoriesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesCollectionViewCell.swift
+//  Memento
+//
+//  Created by twoinosk on 11/6/17.
+//  Copyright © 2017 memTech. All rights reserved.
+//
+
+import UIKit
+
+class MemoriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var button: UIButton!
+}
